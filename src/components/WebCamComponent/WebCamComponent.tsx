5e360efe1a35cf88109d21b37b4bdae515d052ed
Brijesh Kumar <bk10895@gmail.com>
2024-07-22 14:59:41 +0530

    feat: add WebcamComponent and react-webcam dependency
    - Include react-webcam in project dependencies to handle webcam functionalities
    - Create new WebcamComponent to encapsulate webcam related features
    - Export WebcamComponent from the components module for easier imports

---
+++ src/components/WebCamComponent/WebCamComponent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+
+export const WebcamComponent: React.FC = () => {
+  const webcamRef = React.useRef<Webcam>(null);
+
+  return (
+    <div className='fixed bottom-4 right-4 border border-gray-400 rounded'>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat='image/jpeg'
+        className='w-32 h-32'
+      />
+    </div>
+  );
+};
